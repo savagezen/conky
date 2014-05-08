8cdc9a9e61f9ccb9dabd3ca59d5ff6873fd978b7
Austin <austin.haedicke@gmail.com>
2014-05-08 10:52:44 -0400

    old mint conkies

---
+++ two-noobslab.lua
@@ -0,0 +1,179 @@
+# Original Work - http://www.noobslab.com/2012/07/install-conky-two-gadget-in-ubuntulinux.html
+
+settings_table = {
+    
+    {
+        name='cpu',
+        arg='cpu0',
+        max=100,
+        bg_colour=0x000000,
+        bg_alpha=0.2,
+        fg_colour=0x6cde4b,
+        fg_alpha=0.8,
+        x=215, y=100,
+        radius=80,
+        thickness=20,
+        start_angle=0,
+        end_angle=270
+    },
+    {
+        name='memperc',
+        arg='',
+        max=100,
+        bg_colour=0x000000,
+        bg_alpha=0.2,
+        fg_colour=0x6cde4b,
+        fg_alpha=0.8,
+        x=215, y=100,
+        radius=60,
+        thickness=15,
+        start_angle=0,
+        end_angle=270
+    },
+    {
+        name='fs_used_perc',
+        arg='/',
+        max=100,
+        bg_colour=0x000000,
+        bg_alpha=0.2,
+        fg_colour=0x6cde4b,
+        fg_alpha=0.8,
+        x=195, y=495,
+        radius=30,
+        thickness=15,
+        start_angle=0,
+        end_angle=270
+    },
+    {
+        name='fs_used_perc',
+        arg='/home',
+        max=100,
+        bg_colour=0x000000,
+        bg_alpha=0.2,
+        fg_colour=0x6cd34b,
+        fg_alpha=0.8,
+        x=195, y=495,
+        radius=15,
+        thickness=10,
+        start_angle=0,
+        end_angle=270
+    },
+    {
+        name='time',
+        arg='%S',
+        max=60,
+        bg_colour=0x000000,
+        bg_alpha=0.2,
+        fg_colour=0x6cde4b,
+        fg_alpha=0.8,
+        x=90, y=200,
+        radius=40,
+        thickness=12,
+        start_angle=180,
+        end_angle=450
+    },
+     {
+        name='time',
+        arg='%M',
+        max=60,
+        bg_colour=0x000000,
+        bg_alpha=0.2,
+        fg_colour=0x6cde4b,
+        fg_alpha=0.8,
+        x=90, y=200,
+        radius=25,
+        thickness=10,
+        start_angle=180,
+        end_angle=450
+    },
+    {
+        name='time',
+        arg='%H',
+        max=24,
+        bg_colour=0x000000,
+        bg_alpha=0.2,
+        fg_colour=0x6cde4b,
+        fg_alpha=0.8,
+        x=90, y=200,
+        radius=10,
+        thickness=8,
+        start_angle=180,
+        end_angle=540
+    },
+       {
+        name='battery_percent',
+        arg='BAT0',
+        max=100,
+        bg_colour=0x000000,
+        bg_alpha=0.2,
+        fg_colour=0x6cde4b,
+        fg_alpha=0.8,
+        x=300, y=460,
+        radius=18,
+        thickness=10,
+        start_angle=0,
+   end_angle=360
+    },
+}
+
+require 'cairo'
+
+function rgb_to_r_g_b(colour,alpha)
+   return ((colour / 0x10000) % 0x100) / 255., ((colour / 0x100) % 0x100) / 255., (colour % 0x100) / 255., alpha
+end
+
+function draw_ring(cr,t,pt)
+   local w,h=conky_window.width,conky_window.height
+   
+   local xc,yc,ring_r,ring_w,sa,ea=pt['x'],pt['y'],pt['radius'],pt['thickness'],pt['start_angle'],pt['end_angle']
+   local bgc, bga, fgc, fga=pt['bg_colour'], pt['bg_alpha'], pt['fg_colour'], pt['fg_alpha']
+
+   local angle_0=sa*(2*math.pi/360)-math.pi/2
+   local angle_f=ea*(2*math.pi/360)-math.pi/2
+   local t_arc=t*(angle_f-angle_0)
+
+   -- Draw background ring
+
+   cairo_arc(cr,xc,yc,ring_r,angle_0,angle_f)
+   cairo_set_source_rgba(cr,rgb_to_r_g_b(bgc,bga))
+   cairo_set_line_width(cr,ring_w)
+   cairo_stroke(cr)
+   
+   -- Draw indicator ring
+
+   cairo_arc(cr,xc,yc,ring_r,angle_0,angle_0+t_arc)
+   cairo_set_source_rgba(cr,rgb_to_r_g_b(fgc,fga))
+   cairo_stroke(cr)      
+end
+
+function conky_ring_stats()
+   local function setup_rings(cr,pt)
+      local str=''
+      local value=0
+      
+      str=string.format('${%s %s}',pt['name'],pt['arg'])
+      str=conky_parse(str)
+      
+      value=tonumber(str)
+      if value == nil then value = 0 end
+      pct=value/pt['max']
+      
+      draw_ring(cr,pct,pt)
+   end
+
+   if conky_window==nil then return end
+   local cs=cairo_xlib_surface_create(conky_window.display,conky_window.drawable,conky_window.visual, conky_window.width,conky_window.height)
+   
+   local cr=cairo_create(cs)   
+   
+   local updates=conky_parse('${updates}')
+   update_num=tonumber(updates)
+   
+   if update_num>5 then
+      for i in pairs(settings_table) do
+         setup_rings(cr,settings_table[i])
+      end
+   end
+   cairo_surface_destroy(cs)
+  cairo_destroy(cr)
+end
