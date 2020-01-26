608423f05e9af841155ea0e3a5d597d102b78014
Austin Haedicke <austin.haedicke@gmail.com>
2020-01-26 10:59:04 -0500

    monthly push: 2020-01

--- popup_cal.sh
+++ popup_cal.sh
@@ -8,7 +8,7 @@ BG='#222222'
  # Title
  echo "$(cal | head -n 1)"
  # The following lines go to slave window
- echo "$(cal)"
+ echo "$(cal | tail -n 7)"
 ) | dzen2 -p '5' -x "585" -y "19" -w "200" -l "7" -sa 'c' -ta 'c' -fg $FG -bg $BG\
     -title-name 'popup_cal' -e 'onstart=uncollapse;button1=exit;button3=exit' && sleep 0.1s && transset-df --name popup_cal 0.5
 
